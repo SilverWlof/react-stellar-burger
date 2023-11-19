817133927f4c9a1e6edbaf73660000075370964b
SilverWlof <wlof55Gmail.com>
2023-11-19 12:42:22 +0400

    step1

--- src/components/centred-control/centred-control.js
+++
@@ -1,5 +0,0 @@
-import styles from "./centred-control.module.css";
-const CentredControl = (props) => {
-  return <div className={styles.Container}>{props.children}</div>;
-};
-export default CentredControl;
--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const apiConfig = {
-  baseUrl: "https://norma.nomoreparties.space/api",
-  headers: { "Content-Type": "application/json; charset=UTF-8" },
-};
